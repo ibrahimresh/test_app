ebe637ff17925d149d38c3c4f862c2b421fdbf0f
i.resheh <i.resheh@namaa-solutions.com>
2025-03-26 12:50:04 +0300

    first commit

--- lib/features/add_invoice_items/view/add_invoice_items_screen.dart
+++ lib/features/add_invoice_items/view/add_invoice_items_screen.dart
@@ -1,4 +1,8 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_test_app/core/constants/app_colors.dart';
+import 'package:flutter_test_app/core/constants/app_vectors.dart';
 import 'package:flutter_test_app/features/add_invoice_items/view/components/invoice_item_widget.dart';
 import 'package:flutter_test_app/features/shared/widgets/back_icon_widget.dart';
 import 'package:flutter_test_app/features/shared/widgets/notifications_icon_widget.dart';
@@ -22,10 +26,47 @@ class AddInvoiceItemsScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: ListView.builder(
-        itemCount: 4,
-        padding: const EdgeInsets.only(top: 24),
-        itemBuilder: (context, index) => const InvoiceItemWidget(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              padding: const EdgeInsets.only(top: 24),
+              itemBuilder: (context, index) => const InvoiceItemWidget(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: SizedBox(
+              width: double.infinity,
+              height: 46,
+              child: DottedBorder(
+                color: AppColors.greyColor,
+                radius: const Radius.circular(12),
+                borderType: BorderType.RRect,
+                strokeWidth: 0.5,
+                dashPattern: const [4,3],
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'إضافة مادة',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      SvgPicture.asset(AppVectors.addCounter, width: 32, height: 32),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
